255b5c96707191b4ebba744fbf2d204e19ef58c0
SAKURA Ayame <137654183+Iris2664@users.noreply.github.com>
2024-07-07 03:09:50 +0900

    file add

---
+++ 2024/003.py
@@ -0,0 +1,11 @@
+str1 =  int ( input("input a number 1:"))
+str2 =  int ( input("input a number 2:"))
+
+sum1 = str1 + str2
+print(sum1)
+
+str3 =  int ( input("input a number 3:"))
+
+sum2 = sum1 + str3
+
+print(sum2)
